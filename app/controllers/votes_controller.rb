bc01b32c158ab868f6ad1da5b913768f974ba97a
patriciacarbajal <carbajal.patricia@gmail.com>
2014-12-13 16:09:12 -0500

    now users can't downvote more than once

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,21 +5,23 @@ class VotesController < ApplicationController
   end
 
   def create
-    @post = Post.find_by params[:id]
+    @post = Post.find params[:post]
     @vote = @post.votes.build voteable_id: @post.id, user_id: current_user.id
     if @vote.save
-      vote_limit(@post, current_user)
+      redirect_to category_post_path(@post.category_id, @post.id)
+    else
       redirect_to category_post_path(@post.category_id, @post.id)
     end
   end
 
 
-  def destroy
-    @post = Post.find params[:id]
-    vote_limit(@post, current_user)
-    @post.votes.last.destroy
-    redirect_to category_post_path(@post.category_id, @post.id)
 
+  def destroy
+    @post = Post.find params[:post]
+    if !@post.votes.empty?
+      @post.votes.last.destroy if @post.votes.any? { |vote| vote.id != current_user.id}
+    end
+    redirect_to category_post_path(@post.category_id, @post.id)
   end
   # private
   # def vote_params
