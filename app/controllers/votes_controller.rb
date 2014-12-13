d4535641537e2fd31e0f3f27879f9720115c2cac
patriciacarbajal <carbajal.patricia@gmail.com>
2014-12-13 18:42:01 -0500

    upvoting and downvoting now work perfectly

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,26 +6,29 @@ class VotesController < ApplicationController
 
   def create
     @post = Post.find params[:post]
-    @vote = @post.votes.build voteable_id: @post.id, user_id: current_user.id
-    if @vote.save
-      redirect_to category_post_path(@post.category_id, @post.id)
-    else
-      redirect_to category_post_path(@post.category_id, @post.id)
-    end
-  end
-
-
-
-  def destroy
-    @post = Post.find params[:post]
-    if !@post.votes.empty?
-      @post.votes.last.destroy if @post.votes.any? { |vote| vote.id != current_user.id}
+    @upvote = @post.votes.find_or_initialize_by(user_id: current_user.id)
+    @upvote.update_attributes(postive_vote: true)
+    if @upvote.valid?
+      vote_count(@post)
     end
     redirect_to category_post_path(@post.category_id, @post.id)
   end
-  # private
-  # def vote_params
-  #   params.require(:vote).permit(:voteable_id, :user_id)
-  # end
+
+  def destroy
+    @post = Post.find params[:post]
+    @downvote = @post.votes.find_or_initialize_by(user_id: current_user.id)
+    @downvote.update_attributes(postive_vote: false)
+    if @downvote.valid?
+      vote_count(@post)
+    end
+    redirect_to category_post_path(@post.category_id, @post.id)
+  end
+
+  private
+
+  def vote_count(post)
+    count = post.votes.where(postive_vote: true).count - post.votes.where(postive_vote: false).count
+    post.update_attributes(vote_count: count)
+  end
 
 end
