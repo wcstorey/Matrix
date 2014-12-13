ba6e8fda3697c4d81561c801cbbb46136c5a164d
William Storey <wcstorey@gmail.com>
2014-12-13 17:56:35 -0500

    may edit user profile. validates updates. added user validations:password presence required. password confirmation must match password.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,19 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+def update
+    @user = current_user
+    @user.assign_attributes(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      session[:error] = @user.errors.full_messages
+      render :edit
+    end
+end
+
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:id, :username, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :password, presence: true
+  # validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
   has_many  :posts
   has_many  :comments
 
