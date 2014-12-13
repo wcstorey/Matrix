a8a70eddf766fcd7f3cfd6a686f1c8728e9d48ec
ZahidKarim <zahid.jethani@gmail.com>
2014-12-13 11:03:56 -0500

    change a route in the admin show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,12 +4,16 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
-  def new
-    @category = Category.new
-  end
-
   def create
-
+    p "*"*30
+    p category_params
+    @admin = Admin.find(session[:admin_id])
+    @admin.categories.create(category_params[:name])
+    redirect_to root_path
   end
 
+private
+  def category_params
+    params.require(:category).permit(:name, :admin_id)
+  end
 end
