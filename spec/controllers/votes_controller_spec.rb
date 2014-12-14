c8f3e2a027963b49c1e35bce05ccc33d9873e056
patriciacarbajal <carbajal.patricia@gmail.com>
2014-12-14 16:33:45 -0500

    first vote controller test passing

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
-describe VotesController do
-  let(:test_category) {FactoryGirl.create :category}
-  let(:test_post) {FactoryGirl.create :post}
-  let(:test_user) {FactoryGirl.create :user}
-  let(:test_vote) {test_post.votes.create :vote}
+RSpec.describe VotesController, type: :controller do
 
 
+  let(:test_category) { FactoryGirl.create :category }
+  let(:test_post) { FactoryGirl.create :post }
+  let(:test_user) { FactoryGirl.create :user }
+  let(:test_vote) { test_post.votes.create :vote }
 
 
   describe "#create" do
+    
     it "creates the vote if valid params" do
+      allow(controller).to receive(:current_user) { test_user }
       expect {
         post :create,
-        :category_id => test_category,
-        :post_id => test_category.id,
-        :user_id => test_user.id
+          post: test_post.id
       }.to change { Vote.count }.by(1)
     end
   end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,16 +1,18 @@
 FactoryGirl.define do
   factory :category do
-    name { Faker::Lorem.word }
+    name  Faker::Lorem.word 
   end
 
   factory :post do
-    title { Faker::Lorem.word }
-    content {Faker::Lorem.sentence}
+    title  Faker::Lorem.word 
+    content Faker::Lorem.sentence
+    category_id 1
   end
 
   factory :user do
-    username {Faker::Internet.user_name}
-    email {Faker::Internet.email}
-    password {Faker::Internet.password}
+    username Faker::Internet.user_name
+    email Faker::Internet.email
+    password "pass123"
+    password_confirmation "pass123"
   end
 end
