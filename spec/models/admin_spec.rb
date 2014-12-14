f37d08e0e03ba631147882c8700a4dec53182fc9
ZahidKarim <zahid.jethani@gmail.com>
2014-12-14 15:53:39 -0500

    added test for password validation

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe Admin, :type => :model do
     end
   end
 
+  context "when a admin goes to signup" do
+    it "is invalid without an password" do
+      admin = Admin.new(password: nil)
+      admin.valid?
+      expect(admin.errors[:password]).to include("can't be blank")
+    end
+  end
+
 
 
 
