9024da149612848d5edede5b179009582e6fdebb
patriciacarbajal <carbajal.patricia@gmail.com>
2014-12-14 18:13:49 -0500

    tests passes to for downvote feature

---
+++ spec/features/votes_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "Vote functionality" do
+  let(:user) { FactoryGirl.create(:user) }
+  let!(:category) { FactoryGirl.create(:category) }
+  let!(:post) { FactoryGirl.create(
+      :post,
+      user: user,
+      category: category,
+      content: "test" )}
+
+   scenario "user logged in search for test" do
+    sign_in_as! user
+    visit category_post_path(post.category_id, post.id)
+    click_on 'Downvote'
+    expect(post.votes.last.postive_vote).to be false       
+    end
+end
