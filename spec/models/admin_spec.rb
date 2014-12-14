e157c5c247c684e51d8d9d29c0f5b99e254d034d
ZahidKarim <zahid.jethani@gmail.com>
2014-12-14 15:46:40 -0500

    added an admin test for user name

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Admin, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
-
+  it "is invalid without a username" do
+    admin = Admin.new(username: nil)
+    admin.valid?
+    expect(admin.errors[:username]).to include("can't be blank")
+  end
 end
