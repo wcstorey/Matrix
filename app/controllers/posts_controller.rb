209fd333e1898e6766c6ae070709e5b7cd6c82aa
Beatrix "Betsy" Carroll <beatrixcarroll@gmail.com>
2014-12-14 19:26:20 -0500

    refactored some logic in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,12 +21,8 @@ class PostsController < ApplicationController
     @post = category.posts.new(post_params)
     @post.update(user_id: current_user.id)
     if @post.save
-      if @post.parent.nil?
-        redirect_to category_post_path id: @post
-      else
         @original_page = Post.find(params[:post][:page_id])
         redirect_to category_post_path id: @original_page
-      end
     else
       render :new
     end
